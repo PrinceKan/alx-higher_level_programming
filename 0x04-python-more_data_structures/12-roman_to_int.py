412952f0482f020a2e805eba6bcf268a13156bce
Moussa KANE <princekan9.mk@gmail.com>
2022-12-08 00:42:35 +0000

    Roman to Integer

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    if isinstance(roman_string, str):
+        result = 0
+        convert_num = 0
+        roman_base_numbers = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
+                        'C': 100, 'D': 500, 'M': 1000}
+        for idx in reversed(roman_string):
+            convert_num = roman_base_numbers[idx]
+            if result < convert_num * 5:
+                result = result + convert_num
+            else:
+                result = result - convert_num
+        return result
+    else:
+        return 0
