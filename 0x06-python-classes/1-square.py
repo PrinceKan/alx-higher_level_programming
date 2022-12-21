5ff67d51743fc00e03720c9d280fde77b61116aa
Moussa KANE <princekan9.mk@gmail.com>
2022-12-21 16:05:24 +0000

    correct docstring Square with size

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -3,11 +3,16 @@
 """Defines a square"""
 
 
-class Square():
+class Square():i
+    """represent asquare"""
+
     def __init__(self, size):
-        """Initializes the attributes of the instance
-           Args:
-           self : reference to the instance of the class
-           size (int): integer that represents the square's size
-        """
+    """Initializes the attributes of the instance
+
+       Args:
+       self : reference to the instance of the class
+       size (int): integer that represents the square's sizei
+
+    """
+
         self.__size = size
