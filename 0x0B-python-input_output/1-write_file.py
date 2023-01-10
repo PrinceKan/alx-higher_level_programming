dbca2ece5af65095425cac34a86d220c88196a49
Moussa KANE <princekan9.mk@gmail.com>
2023-01-10 18:04:25 +0000

    Write to a file

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+""" Define the function write_file """
+
+
+def write_file(filename="", text=""):
+    """write_file method that Writes a string to a text file (UTF8)
+       Create the file if doesn't exist and overwrite the content
+       of the file if it already exists.
+       Args:
+            filename="": Handle the file name 
+            text="": Handle the text into the file
+
+       Return: the number of characters written
+    """
+    with open(filename, "w", encoding="utf-8") as f:
+        return (f.write(text))
