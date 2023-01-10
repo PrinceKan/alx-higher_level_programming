71109d27d81269a9008a7d40ec00d70920f07f80
Moussa KANE <princekan9.mk@gmail.com>
2023-01-10 21:03:16 +0000

    Revert "Load, add, save"
    This reverts commit 588b27eb965eb4cc3b7fd518226d034a899d43cc.

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -4,6 +4,8 @@ import sys
 save = __import__('5-save_to_json_file').save_to_json_file
 load = __import__('6-load_from_json_file').load_from_json_file
 
+
+""" Adds all arguments to a Python list, and then save them to a file """
 try:
     list = load("add_item.json")
 except Exception:
