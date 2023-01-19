445c108f98b79fa6143e68d0a858efb3758d710a
Moussa KANE <princekan9.mk@gmail.com>
2023-01-19 19:20:49 +0000

    JSON string to file

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-"""Creating the class named base that will be the \"base\" of all other\
+"""Creating the class named base that will be the “base” of all other\
    classes in this project
 """
 import json
@@ -25,14 +25,30 @@ class Base:
     @staticmethod
     def to_json_string(list_dictionaries):
         """to_json_string method static method
-
            Args:
                list_dictionarieslist_dictionaries: is a list of dictionaries
-
            Return: a string \"[]\" or JSON string representation of\
                    list_dictionaries
         """
-        if list_dictionaries is None or len(list_dictionaries) is 0:
+        if list_dictionaries is None or len(list_dictionaries) == 0:
             return "[]"
         else:
             return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """save_to_filethat  writes the JSON string representation
+            of list_objs to a file
+           Args:
+               cls: is the class
+               list_objs: is a list of instances who inherits of Base
+        """
+        filename = cls.__name__ + ".json"
+        with open(filename, "w") as fl:
+            index = []
+            if list_objs is not None:
+                for item in list_objs:
+                    item = item.to_dictionary()
+                    index.append(item)
+            dico = cls.to_json_string(index)
+            fl.write(dico)
