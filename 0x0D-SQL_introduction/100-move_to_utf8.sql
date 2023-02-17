c4ebe5cc4cb04a632ee0e20e046e3e1561f51eb2
PrinceKan <princekan9.mk@gmail.com>
2023-02-17 01:10:52 +0000

    Go to UTF8

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,6 +1,5 @@
--- script that converts hbtn_0c_0 database to UTF8 
--- (utf8mb4, collate utf8mb4_unicode_ci) 
--- converting all of the following to UTF8
+-- script that converts hbtn_0c_0 database to UTF8 (utf8mb4,
+--collate utf8mb4_unicode_ci) converting all of the following to UTF8
 ALTER database CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 ALTER TABLE first_table 
 CONVERT TO CHARACTER 
