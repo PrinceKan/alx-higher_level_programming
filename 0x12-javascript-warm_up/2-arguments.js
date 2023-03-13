6ec3dedde26257b2dade770035673281a670d21d
PrinceKan <princekan9.mk@gmail.com>
2023-03-13 23:41:31 +0000

    Arguments

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 
-const args = process.argv.length;
-if (args === 2) {
+args = process.argv.length;
+if (args <= 2) {
   console.log('No argument');
 } else if (args === 3) {
-  console.log('No argument');
+  console.log('Arguments found');
 } else {
   console.log('Arguments found');
 }
