6cee35f8b056508bec7b6c892d03085ced8ab5a5
PrinceKan <princekan9.mk@gmail.com>
2023-03-20 01:46:30 +0000

    Contains

---
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+""" lists all State objects from the database hbtn_0e_6_usa """
+
+from model_state import Base, State
+from sys import argv
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
+
+def list_states():
+    """ list_states function that uses to list the states from the database """
+    mysql_username = argv[1]
+    mysql_password = argv[2]
+    database_name = argv[3]
+
+    db_connect = f"mysql+mysqldb://{mysql_username}:{mysql_password}\
+        @localhost:3306/{database_name}"
+
+    engine = create_engine(db_connect, pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    session = sessionmaker(bind=engine)
+    Session = session()
+
+    states = Session.query(State).filter(State.name.contains('a'))
+    states = states.order_by(State.id)
+    for state in states:
+        print(f"{state.id}: {state.name}")
+
+    Session.close()
+
+
+if __name__ == "__main__":
+    list_states()
