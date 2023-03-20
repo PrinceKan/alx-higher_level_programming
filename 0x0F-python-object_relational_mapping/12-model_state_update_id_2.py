d6afd3906e778a47ff6db5410bf53efaf691b2de
PrinceKan <princekan9.mk@gmail.com>
2023-03-20 02:33:57 +0000

    Update a state

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+""" lists all State objects from the database hbtn_0e_6_usa """
+
+from model_state import Base, State
+from sys import argv
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
+
+def list_states():
+    """ list_states function that uses to list the states from the database """
+    mysql_username = argv[1]
+    mysql_password = argv[2]
+    database_name = argv[3]
+
+    db_connect = f"mysql+mysqldb://{mysql_username}:{mysql_password}\
+        @localhost:3306/{database_name}"
+
+    engine = create_engine(db_connect, pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    session = sessionmaker(bind=engine)
+    Session = session()
+
+    new_state = Session.query(State).filter(State.id == 2).one()
+    new_state_name = "New Mexico"
+    Session.commit()
+
+    Session.close()
+
+
+if __name__ == "__main__":
+    list_states()
