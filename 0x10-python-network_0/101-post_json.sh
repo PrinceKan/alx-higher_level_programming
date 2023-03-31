2aceba81eccb821eb3aca938bef115ba54da0e26
PrinceKan <princekan9.mk@gmail.com>
2023-03-31 01:17:36 +0000

    Revert "cURL a JSON file"
    This reverts commit 7569a0bf16ffffc82f668c1d78afdd298b09b512.

--- 0x10-python-network_0/101-post_json.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# Bash script that sends a JSON POST request to a URL passed as the first argument, and displays the body of the response
-curl -sX POST -H "Content-Type: application/json" --data '{"key1":"value1","key2":"value2"}' 
