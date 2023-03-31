14a5cfd60ddb51a657d0ef86e6086135720fd110
PrinceKan <princekan9.mk@gmail.com>
2023-03-31 01:37:33 +0000

    Catch me if you can!

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Bash script that makes a request to 0.0.0.0:5000/catch_me that causes the server to respond with a message containing,in the body of the response
-curl -s -X PUT -H "Content-Type: text/plain" --data "You got me!" 0.0.0.0:5000/catch_me >/dev/null
+curl -s 0.0.0.0:5000/catch_me | sed -n 's/.*\(You got me!\).*/\1/p'1
