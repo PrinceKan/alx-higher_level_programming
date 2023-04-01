446b97da2b5617bcc1b826bcc5050ce8e8749448
PrinceKan <princekan9.mk@gmail.com>
2023-04-01 02:01:20 +0000

    Search API

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""sends a POST request to http://0.0.0.0:5000/search_user 
+   with the letter as a paramete
+"""
+from sys import argv
+import requests
+
+
+if __name__ == "__main__":
+    url = "http://0.0.0.0:5000/search_user"
+    if len(argv) != 2:
+        q = ""
+    else:
+        q = argv[1]
+    values = {'q': q}
+    r = requests.post(url, values)
+    try:
+        json_content = r.json()
+    except as e:
+        print("Not a valid JSON")
+    else:
+        if len(json_content) < 1:
+            print("No result")
+        else:
+            print(f"[{json_content['id']}] {json_content['name']}")
